22a9316b1896a247b2b33f19f2fbf581f1d9290b
linliangyi2005 <linliangyi2005@93b55510-f27f-11dd-a295-0b7b0935bf94>
2010-09-08 04:48:13 +0000

    重构LetterSegmenter，解决对数字切分的小bug

--- src/org/wltea/analyzer/seg/LetterSegmenter.java
+++ src/org/wltea/analyzer/seg/LetterSegmenter.java
@@ -26,24 +26,39 @@ public class LetterSegmenter implements ISegmenter {
 	 */
 	private int start;
 	/*
-	 * 字母起始位置
-	 */
-	private int letterStart;
-	/*
 	 * 记录词元结束位置
 	 * end记录的是在词元中最后一个出现的Letter但非Sign_Connector的字符的位置
 	 */
 	private int end;
+	
+	/*
+	 * 字母起始位置
+	 */
+	private int letterStart;
+
 	/*
 	 * 字母结束位置
 	 */
 	private int letterEnd;
+	
+	/*
+	 * 阿拉伯数字起始位置
+	 */
+	private int numberStart;
+	
+	/*
+	 * 阿拉伯数字结束位置
+	 */
+	private int numberEnd;
+
 	
 	public LetterSegmenter(){
 		start = -1;
 		end = -1;
 		letterStart = -1;
 		letterEnd = -1;
+		numberStart = -1;
+		numberEnd = -1;
 	}
 	
 	/* (non-Javadoc)
@@ -54,68 +69,55 @@ public class LetterSegmenter implements ISegmenter {
 		//读取当前位置的char	
 		char input = segmentBuff[context.getCursor()];
 		
+		boolean bufferLockFlag = false;
+		//处理混合字母
+		bufferLockFlag = this.processMixLetter(input, context) || bufferLockFlag;
+		//处理英文字母
+		bufferLockFlag = this.processEnglishLetter(input, context) || bufferLockFlag;
+		//处理阿拉伯字母
+		bufferLockFlag = this.processPureArabic(input, context) || bufferLockFlag;
+		
+		//判断是否锁定缓冲区
+		if(bufferLockFlag){
+			//对缓冲区解锁
+			context.unlockBuffer(this);
+		}else{
+			context.lockBuffer(this);
+		}
+	}
+	
+	/**
+	 * 处理数字字母混合输出
+	 * 如：windos2000 | linliangyi2005@gmail.com
+	 * @param input
+	 * @param context
+	 * @return
+	 */
+	private boolean processMixLetter(char input , Context context){
+		boolean needLock = false;
+		
 		if(start == -1){//当前的分词器尚未开始处理字符			
 			if(isAcceptedCharStart(input)){
 				//记录起始指针的位置,标明分词器进入处理状态
 				start = context.getCursor();
 				end = start;
-				if(CharacterHelper.isEnglishLetter(input)){
-					letterStart =  context.getCursor();
-					letterEnd = letterStart;
-				}
 			}
 			
 		}else{//当前的分词器正在处理字符			
 			if(isAcceptedChar(input)){
-				//输入是英文字符
-				if(CharacterHelper.isEnglishLetter(input)){
-					if(letterStart == -1){//设置英文单词起始位置
-						letterStart = context.getCursor();
-					}
-					letterEnd = context.getCursor();
-					
-				}else if(letterStart > -1 && letterEnd > -1){
-					//遇到非英文字符，如果先前存在有英文单词，则切分,重置letterStart ，letterEnd 但不重置start ，end
-					if(letterStart > -1 && letterEnd > -1){
-						//生成已切分的词元
-						Lexeme engLexeme = new Lexeme(context.getBuffOffset() , letterStart , letterEnd - letterStart + 1 , Lexeme.TYPE_LETTER);
-						context.addLexeme(engLexeme);
-						letterStart = -1;
-						letterEnd = -1;
-					}
-				}
-				
-				//输入连接符
-				if(isLetterConnector(input)){
-					//遇到分割符，输出词元，但不重置start ，end
-					Lexeme newLexeme = new Lexeme(context.getBuffOffset() , start , end - start + 1 , Lexeme.TYPE_LETTER);
-					context.addLexeme(newLexeme);
-					
-				}else{
+				//输入不是连接符
+				if(!isLetterConnector(input)){
 					//记录下可能的结束位置，如果是连接符结尾，则忽略
-					end = context.getCursor();
+					end = context.getCursor();					
 				}
 				
 			}else{
 				//生成已切分的词元
 				Lexeme newLexeme = new Lexeme(context.getBuffOffset() , start , end - start + 1 , Lexeme.TYPE_LETTER);
-//				if(!Dictionary.isStopWord(segmentBuff , newLexeme.getBegin() , newLexeme.getLength())){
 				context.addLexeme(newLexeme);
-//				}
 				//设置当前分词器状态为“待处理”
 				start = -1;
 				end = -1;
-				
-				//输出英文单词
-				if(letterStart > -1 && letterEnd > -1){
-					//生成已切分的词元
-					Lexeme engLexeme = new Lexeme(context.getBuffOffset() , letterStart , letterEnd - letterStart + 1 , Lexeme.TYPE_LETTER);
-//					if(!Dictionary.isStopWord(segmentBuff , engLexeme.getBegin() , engLexeme.getLength())){
-					context.addLexeme(engLexeme);
-//					}
-					letterStart = -1;
-					letterEnd = -1;
-				}	
 			}			
 		}
 		
@@ -124,33 +126,123 @@ public class LetterSegmenter implements ISegmenter {
 			if(start != -1 && end != -1){
 				//生成已切分的词元
 				Lexeme newLexeme = new Lexeme(context.getBuffOffset() , start , end - start + 1 , Lexeme.TYPE_LETTER);
-//				if(!Dictionary.isStopWord(segmentBuff , newLexeme.getBegin() , newLexeme.getLength())){
 				context.addLexeme(newLexeme);
-//				}
 			}
 			//设置当前分词器状态为“待处理”
 			start = -1;
 			end = -1;
-			
-			//输出英文单词
-			if(letterStart > -1 && letterEnd > -1){
-				//生成已切分的词元
-				Lexeme engLexeme = new Lexeme(context.getBuffOffset() , letterStart , letterEnd - letterStart + 1 , Lexeme.TYPE_LETTER);
-//				if(!Dictionary.isStopWord(segmentBuff , engLexeme.getBegin() , engLexeme.getLength())){
-				context.addLexeme(engLexeme);
-//				}
-				letterStart = -1;
-				letterEnd = -1;
-			}			
 		}
 		
 		//判断是否锁定缓冲区
 		if(start == -1 && end == -1){
 			//对缓冲区解锁
-			context.unlockBuffer(this);
+			needLock = false;
 		}else{
-			context.lockBuffer(this);
+			needLock = true;
 		}
+		return needLock;
+	}
+	
+	/**
+	 * 处理纯阿拉伯字符输出
+	 * @param input
+	 * @param context
+	 * @return
+	 */
+	private boolean processPureArabic(char input , Context context){
+		boolean needLock = false;
+		
+		if(numberStart == -1){//当前的分词器尚未开始处理数字字符	
+			if(CharacterHelper.isArabicNumber(input)){
+				//记录起始指针的位置,标明分词器进入处理状态
+				numberStart = context.getCursor();
+				numberEnd = numberStart;
+			}
+		}else {//当前的分词器正在处理数字字符	
+			if(CharacterHelper.isArabicNumber(input)){
+				//记录当前指针位置为结束位置
+				numberEnd =  context.getCursor();
+			}else{
+				//生成已切分的词元
+				Lexeme newLexeme = new Lexeme(context.getBuffOffset() , numberStart , numberEnd - numberStart + 1 , Lexeme.TYPE_LETTER);
+				context.addLexeme(newLexeme);
+				//设置当前分词器状态为“待处理”
+				numberStart = -1;
+				numberEnd = -1;
+			}
+		}
+		
+		//context.getCursor() == context.getAvailable() - 1读取缓冲区最后一个字符，直接输出
+		if(context.getCursor() == context.getAvailable() - 1){
+			if(numberStart != -1 && numberEnd != -1){
+				//生成已切分的词元
+				Lexeme newLexeme = new Lexeme(context.getBuffOffset() , numberStart , numberEnd - numberStart + 1 , Lexeme.TYPE_LETTER);
+				context.addLexeme(newLexeme);
+			}
+			//设置当前分词器状态为“待处理”
+			numberStart = -1;
+			numberEnd = -1;
+		}
+		
+		//判断是否锁定缓冲区
+		if(numberStart == -1 && numberEnd == -1){
+			//对缓冲区解锁
+			needLock = false;
+		}else{
+			needLock = true;
+		}
+		return needLock;		
+	}
+	
+	/**
+	 * 处理纯英文字母输出
+	 * @param input
+	 * @param context
+	 * @return
+	 */
+	private boolean processEnglishLetter(char input , Context context){
+		boolean needLock = false;
+		
+		if(letterStart == -1){//当前的分词器尚未开始处理数字字符	
+			if(CharacterHelper.isEnglishLetter(input)){
+				//记录起始指针的位置,标明分词器进入处理状态
+				letterStart = context.getCursor();
+				letterEnd = letterStart;
+			}
+		}else {//当前的分词器正在处理数字字符	
+			if(CharacterHelper.isEnglishLetter(input)){
+				//记录当前指针位置为结束位置
+				letterEnd =  context.getCursor();
+			}else{
+				//生成已切分的词元
+				Lexeme newLexeme = new Lexeme(context.getBuffOffset() , letterStart , letterEnd - letterStart + 1 , Lexeme.TYPE_LETTER);
+				context.addLexeme(newLexeme);
+				//设置当前分词器状态为“待处理”
+				letterStart = -1;
+				letterEnd = -1;
+			}
+		}
+		
+		//context.getCursor() == context.getAvailable() - 1读取缓冲区最后一个字符，直接输出
+		if(context.getCursor() == context.getAvailable() - 1){
+			if(letterStart != -1 && letterEnd != -1){
+				//生成已切分的词元
+				Lexeme newLexeme = new Lexeme(context.getBuffOffset() , letterStart , letterEnd - letterStart + 1 , Lexeme.TYPE_LETTER);
+				context.addLexeme(newLexeme);
+			}
+			//设置当前分词器状态为“待处理”
+			letterStart = -1;
+			letterEnd = -1;
+		}
+		
+		//判断是否锁定缓冲区
+		if(letterStart == -1 && letterEnd == -1){
+			//对缓冲区解锁
+			needLock = false;
+		}else{
+			needLock = true;
+		}
+		return needLock;			
 	}
 	
 	/**
